3ce817ac1b244e4c8e5262206c8081cc59480325
cjstk7168 <kissob4905@gmail.com>
2019-05-08 17:43:23 +0900

    bug fix

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import "reflect-metadata";
-import { createConnection, createQueryBuilder } from "typeorm";
+import { createConnection, createQueryBuilder, IsNull } from "typeorm";
 import { Post } from "./entity/Post";
 import { CategoryPage } from "./entity/CategoryPage";
 const moment = require('moment');
@@ -14,13 +14,12 @@ createConnection().then(async connection => {
     await connection.getRepository(CategoryPage)
         .createQueryBuilder()
         .where("category_id = :categoryId", { categoryId: 5 })
-        .andWhere("show_at is null or show_at >= now()" )
-        .andWhere("hide_at is null or hide_at <= now()" )
+        .andWhere("(show_at is null or show_at >= now())" )
+        .andWhere("(hide_at is null or hide_at <= now())" )
+        .orderBy("updated_at", "DESC")
+        .limit(1)
+        .getMany()
 
-    order: {
-        updated_at: 'DESC'
-    }
     console.timeEnd("TypeORM Transaction 2")
-
 
 }).catch(error => console.log(error));
